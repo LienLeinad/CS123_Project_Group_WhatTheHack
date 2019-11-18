ec22497e8ac6bc1654f0b627800c8b48a615c831
Robin <robin.c.chua@obf.ateneo.edu>
2019-11-18 15:21:26 +0800

    Add files via upload

--- chkngo/webapp/models.py
+++ chkngo/webapp/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User,AbstractUser
 # Create your models here.
 
 class Restaurant (models.Model):
-    RestoID = models.CharField(max_length = 30, unique = True, primary_key = True, verbose_name = "Restaurant ID")#PK
-    MngID = models.CharField(max_length = 30, unique = True, primary_key = True, verbose_name = "Manager ID")#PK
-	ReviewID = models.ForeignKey(Review, on_delete=models.CASCADE)
+	RestoID = models.CharField(max_length = 30, unique = True, primary_key = True, verbose_name = "Restaurant ID")#PK
+	MngID = models.CharField(max_length = 30, unique = True ,verbose_name = "Manager ID")#make this foreign key
+	ReviewID = models.ForeignKey('Review', on_delete=models.CASCADE)
 	Accommodations = models.CharField(max_length=300, default='not set')
 	Seating = models.CharField(max_length=50, default='not set')
 	Rating = models.IntegerField(editable = True, default = 0)
@@ -13,22 +13,22 @@ class Restaurant (models.Model):
 	WaitTime5_8 = models.IntegerField(editable = True, default = 0)
 	WaitTime9_10 = models.IntegerField(editable = True, default = 0)
 
-    def __str__(self):
-        return str(self.RestoID)
+	def __str__(self):
+		return str(self.RestoID)
 
 class Review(models.Model):
 	ReviewID = models.CharField(max_length = 30, unique = True, primary_key = True, verbose_name = "Review ID")#PK
-    RestoID = models.ForeignKey(Restaurant, on_delete = models.CASCADE)
-    Rating = models.IntegerField(editable = True)
+	RestoID = models.ForeignKey(Restaurant, on_delete = models.CASCADE)
+	Rating = models.IntegerField(editable = True)
 	ReviewDetail = models.CharField(max_length=300, default='no Reviews')
-    def __str__ (self):
-        return str(self.Rating)
+	def __str__ (self):
+		return str(self.Rating)
 
 class CustomUser(AbstractUser):
-    contact = models.CharField(
-        max_length = 11,
-        verbose_name = 'Contact Numbers',
-        default = 'n/a'
-    )
-    def __str__(self):
-        return self.username
+	contact = models.CharField(
+		max_length = 11,
+		verbose_name = 'Contact Numbers',
+		default = 'n/a'
+	)
+	def __str__(self):
+		return self.username
